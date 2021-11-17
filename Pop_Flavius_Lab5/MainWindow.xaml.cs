52b2918514a1d936ad7f3f8d14139b431772f1b4
Flavius Pop <popflavius2000@yahoo.ro>
2021-11-17 18:12:42 +0200

    Laborator7

--- Pop_Flavius_Lab5/MainWindow.xaml.cs
+++ Pop_Flavius_Lab5/MainWindow.xaml.cs
@@ -71,10 +71,14 @@ namespace Pop_Flavius_Lab5
         private void btnAdd_Click(object sender, RoutedEventArgs e)
         {
             action = ActionState.New;
+            SetValidationBinding();
         }
         private void btnEditO_Click(object sender, RoutedEventArgs e)
         {
             action = ActionState.Edit;
+            BindingOperations.ClearBinding(firstNameTextBox, TextBox.TextProperty);
+            BindingOperations.ClearBinding(lastNameTextBox, TextBox.TextProperty);
+            SetValidationBinding();
         }
         private void btnDeleteO_Click(object sender, RoutedEventArgs e)
         {
@@ -343,6 +347,32 @@ namespace Pop_Flavius_Lab5
                                  inv.Color
                              };
             customerOrdersVSource.Source = queryOrder.ToList();
+        }
+        private void SetValidationBinding()
+        {
+            Binding firstNameValidationBinding = new Binding();
+            firstNameValidationBinding.Source = customerVSource;
+            firstNameValidationBinding.Path = new PropertyPath("FirstName");
+            firstNameValidationBinding.NotifyOnValidationError = true;
+            firstNameValidationBinding.Mode = BindingMode.TwoWay;
+            firstNameValidationBinding.UpdateSourceTrigger =
+           UpdateSourceTrigger.PropertyChanged;
+            //string required
+            firstNameValidationBinding.ValidationRules.Add(new StringNotEmpty());
+            firstNameTextBox.SetBinding(TextBox.TextProperty,
+           firstNameValidationBinding);
+            Binding lastNameValidationBinding = new Binding();
+            lastNameValidationBinding.Source = customerVSource;
+            lastNameValidationBinding.Path = new PropertyPath("LastName");
+            lastNameValidationBinding.NotifyOnValidationError = true;
+            lastNameValidationBinding.Mode = BindingMode.TwoWay;
+            lastNameValidationBinding.UpdateSourceTrigger =
+           UpdateSourceTrigger.PropertyChanged;
+            //string min length validator
+            lastNameValidationBinding.ValidationRules.Add(new
+           StringMinLengthValidator());
+            lastNameTextBox.SetBinding(TextBox.TextProperty,
+           lastNameValidationBinding); //setare binding nou
         }
 
     }
---
+++ Pop_Flavius_Lab5/Validation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Pop_Flavius_Lab5
+{
+    //validator pentru camp required
+    public class StringNotEmpty : ValidationRule
+    {
+        //mostenim din clasa ValidationRule
+        //suprascriem metoda Validate ce returneaza un
+        //ValidationResult
+        public override ValidationResult Validate(object value, System.Globalization.CultureInfo cultureinfo)
+        {
+            string aString = value.ToString();
+            if (aString == "")
+                return new ValidationResult(false, "String cannot be empty");
+            return new ValidationResult(true, null);
+        }
+    }
+    //validator pentru lungime minima a string-ului
+    public class StringMinLengthValidator : ValidationRule
+    {
+        public override ValidationResult Validate(object value,
+        System.Globalization.CultureInfo cultureinfo)
+        {
+            string aString = value.ToString();
+            if (aString.Length < 3)
+                return new ValidationResult(false, "String must have at least 3 characters!");
+        return new ValidationResult(true, null);
+        }
+    }
+
+}
+
